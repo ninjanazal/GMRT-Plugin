a6d6b018e9759589ae788f199ef6e3d1e471927e
Eurico Martins <euricomartinss@gmail.com>
2022-11-18 00:18:32 +0000

    Plugin tab for godot project status

--- plugin.gd
+++ plugin.gd
@@ -1,9 +1,8 @@
 tool
 extends EditorPlugin
 
-var _views : Dictionary = {
+var _view : Dictionary = {
 	"gmtk_btn" : preload("res://addons/GMRT-Plugin/view/GMRTMainButton.tscn").instance(),
-	"board" : preload("res://addons/GMRT-Plugin/view/mainview.tscn").instance()
 };
 
 
@@ -11,19 +10,11 @@ var _views : Dictionary = {
 # Godot API Functions
 # - - - - - - - - - - - - - - -
 func _enter_tree():
-	add_control_to_container(CONTAINER_CANVAS_EDITOR_MENU, _views.gmtk_btn);
-	_views.gmtk_btn.connect_press(self, "_on_pressed");
+	add_control_to_container(CONTAINER_CANVAS_EDITOR_MENU, _view.gmtk_btn);
+	_view.gmtk_btn.setup(self);
 
 func _exit_tree():
-	if(_views.gmtk_btn.is_inside_tree()):
-		remove_control_from_container(CONTAINER_CANVAS_EDITOR_MENU, _views.gmtk_btn);
-	_views.gmtk_btn.free();
-
-# - - - - - - - - - - - - - - -
-func _on_pressed():
-	print(_views.board.is_inside_tree());
-	if(!_views.board.is_inside_tree()):
-		add_control_to_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_SIDE_RIGHT, _views.board);
-	else:
-		remove_control_from_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_SIDE_RIGHT, _views.board);
-	
+	if(_view.gmtk_btn.is_inside_tree()):
+		remove_control_from_container(CONTAINER_CANVAS_EDITOR_MENU, _view.gmtk_btn);
+	_view.gmtk_btn.on_exit_tree();
+	_view.gmtk_btn.free();
--- src/scenes/GMTRTMainButton/GMTRTMainButton.gd
+++ src/scenes/GMTRTMainButton/GMTRTMainButton.gd
@@ -1,9 +1,29 @@
 tool
 extends Control
 
+
+var _plug : EditorPlugin = null;
+
+var _view : Dictionary = {
+	"board" : preload("res://addons/GMRT-Plugin/view/mainview.tscn").instance()
+};
+
 onready var _comp : Dictionary = {
 	"btn" : get_node("HBoxContainer/Button")
 };
 
-func connect_press(target, func_name: String):
-	_comp.btn.connect("pressed", target, func_name);
+func setup(plug):
+	_plug = plug;
+	_comp.btn.connect("pressed", self, "_on_button_press");
+
+func on_exit_tree():
+	if(_view.board.is_inside_tree()):
+		_plug.remove_control_from_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_SIDE_RIGHT, _view.board);
+	_view.board.free();
+
+func _on_button_press():
+	if(!_view.board.is_inside_tree()):
+		_plug.add_control_to_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_SIDE_RIGHT, _view.board);
+	else:
+		_plug.remove_control_from_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_SIDE_RIGHT, _view.board);
+	
